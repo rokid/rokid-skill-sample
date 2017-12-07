f746d68042c64c8cdb3410f226c7d451da067b69
smile-xuc <smile.xuc@gmail.com>
2017-12-07 15:21:37 +0800

    更新我要喝咖啡js示例代码

--- rokid-skill-sample-js-tastecoffee/tastecoffee-js/sample-js/js-engine.js
+++ rokid-skill-sample-js-tastecoffee/tastecoffee-js/sample-js/js-engine.js
@@ -1,3 +1,4 @@
+
 exports.handler = function (event, context, callback) {
     var rokid = Rokid.handler(event, context, callback);
     rokid.registerHandlers(handlers);
@@ -6,27 +7,27 @@ exports.handler = function (event, context, callback) {
 
 var handlers = {
     'ROKID.INTENT.WELCOME': function () {
-        try{
-            this.emit(':tts', { tts: '您好，请问有什么可以帮您。' });
-            this.callback(null);
-        }catch(e){
-            this.callback(e);
+        try {
+            this.setTts({ tts: '您好，请问有什么可以帮您。' });
+            this.emit(':done');
+        } catch (error) {
+            this.emit(':error', error);
         }
     },
     'bestcoffeebar': function () {
-        try{
-            this.emit(':tts', { tts: '我看位于问溪路89号的米萨咖啡就很不错。' });
-            this.callback(null);
-        }catch(e){
-            this.callback(e);
+        try {
+            this.setTts({ tts: '我看位于问溪路89号的米萨咖啡就很不错。' });
+            this.emit(':done');
+        } catch (error) {
+            this.emit(':error', error);
         }
     },
     'nicedrink': function () {
-        try{
-            this.emit(':tts', { tts: '只要是咖啡都很好喝。' });
-            this.callback(null);
-        }catch(e){
-            this.callback(e);
+        try {
+            this.setTts({ tts: '只要是咖啡都很好喝。' });
+            this.emit(':done');
+        } catch (error) {
+            this.emit(':error', error);
         }
     }
 };
